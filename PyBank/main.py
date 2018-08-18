bd7d806152a73b912d6acce95be1c276043aaf59
khrysvaughan <khrywill@gmail.com>
2018-08-18 18:48:42 -0400

    Added changes to main for Net Amount

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,7 @@ resourcefile = os.path.join("Resources", "budget_data.csv")
 with open(resourcefile, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # Read the header row first (skip this part if there is no header)
+    # Read the header row first
     csv_header = next(csvfile)
 
     #Print Header
@@ -17,5 +17,13 @@ with open(resourcefile, newline="") as csvfile:
     #The total number of months included in the dataset
     details = list(csvreader)
     rowcount = len(details)
-
+ 
     print("Total Months: " + str(rowcount))
+
+    #The total net amount of "Profit/Losses" over the entire period
+    netamount = 0
+    for row in details:
+        netamount += int(row[1])
+        
+    
+    print("Total: $" + str(netamount))
