e3654acac2451320853d162efb930540f11f66f2
khrysvaughan <khrywill@gmail.com>
2018-08-24 23:13:51 -0400

    Added average change plus min and max

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,5 +25,43 @@ with open(resourcefile, newline="") as csvfile:
     for row in details:
         netamount += int(row[1])
         
-    
     print("Total: $" + str(netamount))
+
+    #The average change in "Profit/Losses" between months over the entire period
+    # Find change per month
+    diffmonths = 0
+    avgchange = []
+    avgmonths = 0
+    sumchange = 0
+    for i in range(rowcount):
+        if i == 0:
+            avgchange.append(0)
+        else:
+            diffmonths = (int(details[i][1]) - int(details[i-1][1]))
+            avgchange.append(diffmonths)
+            
+    # Add the differences
+    for i in range(rowcount):
+        sumchange += int(avgchange[i])
+
+    # Find the average
+    avgmonths = sumchange/ int(rowcount -1)
+    
+    print("Average Change: $" + str(round(avgmonths,2)))
+
+    #The greatest increase in profits (date and amount) over the entire period
+    greatincvalue = max(avgchange)
+    greatincindex = avgchange.index(greatincvalue)
+    greatincmonth = details[greatincindex][0]
+
+    print("Greatest Increase in Profits: " + greatincmonth + " ($" + str(greatincvalue) + ")")
+
+    #The greatest decrease in losses (date and amount) over the entire period
+    greatdecvalue = min(avgchange)
+    greatdecindex = avgchange.index(greatdecvalue)
+    greatdecmonth = details[greatdecindex][0]
+
+    print("Greatest Decrease in Profits: " + greatdecmonth + " ($" + str(greatdecvalue) + ")")
+
+    #Export a text file with the results
+    
